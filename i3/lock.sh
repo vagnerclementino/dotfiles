52501035ec618f5755061a01006ab71bacccdd9b
Vagner Clementino <vagner.clementino@gmail.com>
2026-04-13 22:35:34 -0300

    fix: lock screen greeter font size 14

--- i3/lock.sh
+++ i3/lock.sh
@@ -89,7 +89,7 @@ i3lock \
     --greeter-text="$GREETER" \
     --greeter-color=bd93f9ff \
     --greeter-font="Hack" \
-    --greeter-size=12 \
+    --greeter-size=14 \
     --greeter-pos="x+w/2:y+h/2+200" \
     --ind-pos="x+w/2:y+h/2" \
     --line-uses-ring \
